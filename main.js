9abc97d81bf1a248e02f37e8c8d005032a9aac89
Tuomas Korpi <tuomas.av.korpi@gmail.com>
2020-08-01 11:39:12 +0300

    asd

--- main.js
+++ main.js
@@ -16,35 +16,55 @@ const tab2 = document.getElementById("tab2");
 const tabback = document.getElementById("tabback");
 const me = document.getElementById("me");
 const outputbox = document.getElementById("outputbox");
-const runico = document.getElementById("runico");
+const runbtn = document.getElementById("runbtn");
 
-
+let menucount = 1;
 
 //FILE ICON CLICK OPEN MENU
 fileico.addEventListener("click", toggle());
 function toggle(){
     return function(){
         //OPEN
-        navmenu.style.display = 'block';
-        console.log("click");
-        code.style.left = '0px';
-        tab1.style.left = '0px';
-        tab2.style.left = '0px';
-        code.style.width = '1571px';
-        output.style.width = '1571px';
+        if (menucount % 2 != 0) {
+            navmenu.style.display = 'block';
+            console.log("click");
+            //move grid
+            code.style.left = '0px';
+            tab1.style.left = '0px';
+            tab2.style.left = '0px';
+            code.style.width = '1571px';
+            output.style.width = '1571px';
+            //border
+            fileico.style.borderLeft = 'solid 6px #D4D4D4';
+        }
+        //CLOSE
+        if(menucount % 2 == 0){
+            navmenu.style.display = 'none';
+            console.log("click");
+            //move grid
+            code.style.left = '-270px';
+            code.style.width = '1841px';
+            tab1.style.left = '-270px';
+            tab2.style.left = '-270px';
+            output.style.width = '1841px';
+            //no border
+            fileico.style.borderLeft = 'solid 6px #333333';
+
+        }
+    menucount ++;    
 }}
 //RUN ICON CLICK CHANGE TXT
 runico.addEventListener("click", runcode());
 function runcode(){
     return function(){
         outputbox.innerHTML = 
-            '<div id="outputtxt"><u>OUTPUT</u><br><br>Hello World! I am Tuomas Korpi from Kuopio, Finland.<br><br>'  
+            '<div id="outputtxt"><span class="outtitle">OUTPUT</span><br><br>Hello World! I am Tuomas Korpi from Kuopio, Finland.<br><br>'  
             +'This website showcases my abilities as web/software developer<br>'
             +'and IoT engineer. Open menu (<span id="smallfile"><img src="icons/smallfile.svg" ></span> ) on the left or click tab on<br>' 
             +'the top to see my projects.</div>';
 
-        runico.innerHTML =
-            '<img src="icons/runbtngrey.svg" >';
+        //button to gray
+        runbtn.style.fill = '#D4D4D4 ';
 }}
 
 
