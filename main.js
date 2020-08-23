0390c51fea5c1f4edbfa546897979b69fff67784
Tuomas Korpi <tuomas.av.korpi@gmail.com>
2020-08-23 20:27:06 +0300

    master

--- main.js
+++ main.js
@@ -138,3 +138,29 @@ function clickTab1(){
 
             
 }}
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+//event listner 
+document.querySelectorAll('.projectModal').forEach(item => {
+    item.addEventListener('click', event => {
+        modal.style.display = "block";
+    })
+  })
+
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
